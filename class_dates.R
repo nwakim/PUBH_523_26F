1709a9a7c62f3d8cb80bec00fa957554ef97f1a7
nwakim <wakim@ohsu.edu>
2026-09-14 21:16:03 -0700

    first commit

---
+++ class_dates.R
@@ -0,0 +1,72 @@
+
+library(dplyr)
+
+# Set the first day of class: "Term begins" date in OHSU academic calendar
+first_day = as.Date("6/22/26", "%m/%d/%y")
+# Set last day of the class: "Term ends" date in OHSU academic calendar
+last_day = as.Date("9/6/26", "%m/%d/%y")
+
+# Create the calendar dates sequence
+cal_dates = seq(from = first_day, to = last_day, by = "day")
+
+## Week 1 dates
+w1d1 = cal_dates[1]
+w1d2 = cal_dates[3]
+pract1 = cal_dates[7]
+
+## Week 2 dates
+w2d1 = cal_dates[8]
+w2d2 = cal_dates[10]
+pract2 = cal_dates[14]
+
+## Week 3 dates
+w3d1 = cal_dates[15]
+w3d2 = cal_dates[17]
+pract3 = cal_dates[21]
+
+## Week 4 dates 
+w4d1 = cal_dates[22]
+w4d2 = cal_dates[24]
+pract4 = cal_dates[28]
+lab1 = cal_dates[28]
+
+## Week 5 dates
+w5d1 = cal_dates[29]
+w5d2 = cal_dates[31]
+pract5 = cal_dates[42]
+
+## Week 6 dates
+w6d1 = cal_dates[36]
+w6d2 = cal_dates[38]
+pract6 = cal_dates[49]
+lab2 = cal_dates[49]
+
+
+## Week 7 dates
+w7d1 = cal_dates[43]
+w7d2 = cal_dates[45]
+pract7 = cal_dates[56]
+lab3 = cal_dates[56]
+
+
+## Week 8 dates
+w8d1 = cal_dates[50]
+w8d2 = cal_dates[52]
+pract8 = cal_dates[63]
+
+## Week 9 dates
+w9d1 = cal_dates[57]
+w9d2 = cal_dates[59]
+pract9 = cal_dates[70]
+lab4 = cal_dates[70]
+
+## Week 10 dates
+w10d1 = cal_dates[64]
+w10d2 = cal_dates[66]
+
+
+## Week 11 dates
+w11d1 = cal_dates[71]
+w11d2 = cal_dates[73]
+proj = cal_dates[77]
+last_day = cal_dates[77]
